2132e7682b571a746137570b5e5f9753e553c03e
Goran Djonovic <goran_djonovic@hotmail.com>
2014-08-12 11:15:07 +0200

    Reminders list and data storage

--- Dugmici/src/com/example/dugmici/AddRemindersActivity.java
+++ Dugmici/src/com/example/dugmici/AddRemindersActivity.java
@@ -2,16 +2,16 @@ package com.example.dugmici;
 
 
 
-import com.example.dugmici.model.Reminder;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.dugmici.model.Reminder;
+
+import eu.execom.locafun.DataStorage;
 
 public class AddRemindersActivity extends Activity {
 
@@ -24,10 +24,12 @@ public class AddRemindersActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-                final EditText detailsText = (EditText) findViewById(R.id.editText2);
-Reminder reminder1 = new Reminder(detailsText.getText().toString(), "");
-                Toast.makeText(AddRemindersActivity.this ,  reminder1.toString() ,  Toast.LENGTH_LONG).show();
-
+                final EditText detailsText = (EditText) findViewById(R.id.taskDetail);
+                final EditText taskText = (EditText) findViewById(R.id.taskName);
+                Reminder reminder = new Reminder(taskText.getText().toString(), detailsText.getText().toString());
+                DataStorage.get(AddRemindersActivity.this).saveReminder(reminder);
+                Toast.makeText(AddRemindersActivity.this ,  reminder.toString() ,  Toast.LENGTH_LONG).show();
+                finish();
             	
             }
         });
--- Dugmici/src/com/example/dugmici/model/Reminder.java
+++ Dugmici/src/com/example/dugmici/model/Reminder.java
@@ -1,53 +1,73 @@
 package com.example.dugmici.model;
 
-import android.R.string;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 
 public class Reminder {
 
-	private String task;
-	private String details;
-	
-	
-	
-	public Reminder (String task, String details) {
-		this.task = task;
-		this.details = details;
-		
-	}
+    private static final String TAG = Reminder.class.getSimpleName();
+    private String task;
+    private String details;
 
+    public Reminder(String task, String details) {
+        this.task = task;
+        this.details = details;
 
+    }
 
-	public String getTask() {
-		return task;
-	}
+    public String getTask() {
+        return task;
+    }
 
+    public void setTask(String task) {
+        this.task = task;
+    }
 
+    public String getDetails() {
+        return details;
+    }
 
-	public void setTask(String task) {
-		this.task = task;
-	}
+    public void setDetails(String details) {
+        this.details = details;
+    }
 
+    @Override
+    public String toString() {
+        return "Reminder [task=" + task + ", details=" + details + "]";
+    }
 
+    public JSONObject toJSON() {
+        JSONObject jsonReminder = new JSONObject();
+        try {
+            jsonReminder.put("task", getTask());
+            jsonReminder.put("details", getDetails());
+        } catch (JSONException e) {
+            Log.e(TAG,"Unable to create JSON");
+        }
+        
+        return jsonReminder;
+    }
 
-	public String getDetails() {
-		return details;
-	}
+    public static Reminder parseJSONString(String jsonReminderString) throws JSONException {
+        JSONObject jsonReminder = new JSONObject(jsonReminderString);
+        Reminder reminder = new Reminder(jsonReminder.getString("task"), jsonReminder.getString("details"));
+        return reminder;
+    }
 
+    public static List<Reminder> parseRemindersList(String jsonRemidersList) throws JSONException {
+        JSONArray jsonArray = new JSONArray(jsonRemidersList);
+        List<Reminder> reminders = new ArrayList<Reminder>(jsonArray.length());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject json = jsonArray.getJSONObject(i);
+            reminders.add(parseJSONString(json.toString()));
+        }
+        return reminders;
+    }
 
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Reminder [task=" + task + ", details=" + details + "]";
-	}
-	
-	
-		
-	
-	
 }
